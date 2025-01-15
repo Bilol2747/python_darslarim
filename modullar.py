ad2ae24ec0c48f31e58acc456effe734140cc8e7
Bilol2747 <ziyadullayevbilol15@gmail.com>
2025-01-15 15:51:57 +0300

    Yangi fayllar qo'shildi
    Modullar darsi

---
+++ modullar.py
@@ -0,0 +1,11 @@
+import random as r
+son=r.randint(0,100)
+print(son) 
+ismlar=['anvar', 'bilol', 'hasan', 'husan']
+ism=r.choice(ismlar)
+print(ism)
+print(r.choice(ismv))
+x=list(range(11))
+print(x)
+r.shuffle(x)
+print(x)
